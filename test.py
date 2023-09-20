317572f64c5e2673723d47a5e0f5736329bbfef1
jaeyeonkk <rlawodus456@naver.com>
2023-09-20 14:50:41 +0900

    얼굴2개 및 없을 때

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, Response
+from flask import Flask, jsonify, render_template, Response
 import cv2
 import dlib
+from datetime import datetime, timedelta
+
 
 app = Flask(__name__)
 
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 detector = dlib.get_frontal_face_detector()
 
+# 전역 변수를 선언하고 초기화합니다
+face_count = 0
+last_face_seen_time = None
+
 def eye_tracking():
+    global face_count, last_face_seen_time
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
@@ -16,6 +23,9 @@ def eye_tracking():
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = detector(gray)
+        face_count = len(faces)
+        if face_count > 0:
+            last_face_seen_time = datetime.now()
 
         for face in faces:
             x1, y1, x2, y2 = (face.left(), face.top(), face.right(), face.bottom())
@@ -35,6 +45,18 @@ def eye_tracking():
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+@app.route('/face_count')
+def face_count_route():
+    global face_count
+    return jsonify(face_count=face_count)
+
+@app.route('/face_info')
+def face_info_route():
+    global face_count, last_face_seen_time
+    no_face_for = (datetime.now() - last_face_seen_time).seconds if last_face_seen_time else 0
+    return jsonify(face_count=face_count, no_face_for=no_face_for)
+
 
 @app.route('/video_feed')
 def video_feed():
