51f8360db006e22ee8dade847de540e51b5b84e1
hhhhh1hhhh <hhhhh_hhhh@naver.com>
2023-11-01 16:14:33 +0900

    student 모델 수정

--- database/models.py
+++ database/models.py
@@ -1,6 +1,7 @@
 import datetime
 import pytz
 from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, TIMESTAMP
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -31,4 +32,11 @@ class Student(Base):
     __tablename__ = "student"
 
     student_id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(50), nullable=False)
+    name = Column(String(50))
+    q_id = Column(Integer, ForeignKey("q_list.q_id"))  # q_id를 외래 키로 설정
+    test_start_time = Column(TIMESTAMP)
+    test_duration = Column(Integer)  # 테스트 소요 시간을 저장할 칼럼
+
+    # Student와 QList 간의 관계 설정
+    q_list = relationship("QList", back_populates="students")
+
