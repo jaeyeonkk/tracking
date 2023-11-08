5449f24618e27ce91084827a9075436ee9b6590f
hhhhh1hhhh <hhhhh_hhhh@naver.com>
2023-11-08 15:53:02 +0900

    회원가입, 로그인, 로그아웃, 로그인한 사용자 표시

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 from app.auth import auth, init_login_manager
 from app.coding_test import coding_test
 from app.config import Config
+from app.csrf_protection import init_csrf
+
 
 app = Flask(__name__, static_folder="app/static")
 app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app", "templates")
@@ -13,12 +16,19 @@ app.register_blueprint(auth)
 app.register_blueprint(coding_test)
 
 init_login_manager(app)
-
+init_csrf(app)
 
 @app.route("/")
-def home():
+def not_logged_home():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     return render_template("main.html")
 
+
+@app.route("/main")
+@login_required
+def home():
+    return render_template("main2.html")
 
 @app.route("/admin")
 def admin():
